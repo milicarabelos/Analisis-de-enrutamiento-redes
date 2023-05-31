6381d4db5d7781dc0edcd76d6d439d05900b28e5
juan-dev <juan-dev@pop-os.localdomain>
2023-05-30 21:40:27 -0300

    adding first test

--- scr/Net.cc
+++ scr/Net.cc
@@ -7,15 +7,56 @@
 
 using namespace omnetpp;
 
-class Net: public cSimpleModule {
+class TransportPacket : public cPacket
+{
+    struct idLists
+    {
+        int id;
+        idLists *next;
+    } idList *list;
+
+public:
+    TransportPacket(const char *name = "TransportPacket", short kind = 2) : cPacket(name, kind) {
+        list = NULL;
+    }
+
+    void addIdList(int id)
+    {
+        // add id list to the end of the current list
+        idList *aux = this->list;
+        while (aux != NULL)
+        {
+            aux = aux->next;
+        }
+        idList *newId = new idList;
+        newId->id = id;
+        newId->next = NULL;
+        aux->next = newId;
+    }
+
+    idList *getIdList()
+    {
+        return list;
+    }
+};
+
+
+class Net : public cSimpleModule
+{
 private:
     cOutVector hopsVector;
-//  cStdDev hopsStats;
+    //  cStdDev hopsStats;
     cOutVector LnkSelectVector;
+    struct idLists
+    {
+        int id;
+        idLists *next;
+    } idList *listIndex;
 
 public:
     Net();
     virtual ~Net();
+
 protected:
     virtual void initialize();
     virtual void finish();
@@ -26,44 +67,76 @@ Define_Module(Net);
 
 #endif /* NET */
 
-Net::Net() {
+Net::Net()
+{
 }
 
-Net::~Net() {
+Net::~Net()
+{
 }
 
-void Net::initialize() {
+void Net::initialize()
+{
     hopsVector.setName("Hops");
     LnkSelectVector.setName("Lnk_Selected");
-//  hopsStats.setName("TotalHops");
+    listIndex = NULL;
+    TransportPacket *pkt = new TransportPacket();
+    pkt->setDestination(this->getParentModule()->getIndex());
+    pkt->addIdList(this->getParentModule()->getIndex());
+    send(pkt, "toLnk$o", 0);
+    //  hopsStats.setName("TotalHops");
 }
 
-void Net::finish() {
-//    recordScalar("AverageHops", hopsStats.getMean());
+void Net::finish()
+{
+    //    recordScalar("AverageHops", hopsStats.getMean());
 }
 
-void Net::handleMessage(cMessage *msg) {
-
-    // All msg (events) on net are packets
-    Packet *pkt = (Packet *) msg;
-
-    // If this node is the final destination, send to App
-    if (pkt->getDestination() == this->getParentModule()->getIndex()) {
-
-        hopsVector.record(pkt->getHopCount());
-        //hopsStats.collect(pkt->getHopCount());
-        send(msg, "toApp$o");
+void Net::handleMessage(cMessage *msg)
+{
+    
+    if (msg->getKind() == 2)
+    {
+        TransportPacket *pkt = (TransportPacket *)msg;
+        
+        this->bubble("recibi tipo 2, id %i", pkt->getIdList()->id);
+        if (pkt->getDestination() == this->getParentModule()->getIndex())
+        {
+            listIndex = pkt->getIdList();
+        }
+        else
+        {
+            pkt->addIdList(this->getParentModule()->getIndex());
+            send(msg, "toLnk$o", 0);
+        }
     }
-    // If not, forward the packet to some else... to who?
-    else {
-        // We send to link interface #0, which is the
-        // one connected to the clockwise side of the ring
-        // Is this the best choice? are there others?
-        pkt->setHopCount(pkt->getHopCount()+1);
+    else
+    {
 
-        //la idea de este vector es ver que porcentaje de veces elegimos mandar por la gate 0 y cuales por
-        //la gate 1 por lo tanto hay que cambiar aca cuando cambiemos la gate elegida
-        LnkSelectVector.record(0);
-        send(msg, "toLnk$o", 0);
+        // All msg (events) on net are packets
+        Packet *pkt = (Packet *)msg;
+
+        // If this node is the final destination, send to App
+
+        if (pkt->getDestination() == this->getParentModule()->getIndex())
+        {
+
+            hopsVector.record(pkt->getHopCount());
+            // hopsStats.collect(pkt->getHopCount());
+            send(msg, "toApp$o");
+        }
+        // If not, forward the packet to some else... to who?
+        else
+        {
+            // We send to link interface #0, which is the
+            // one connected to the clockwise side of the ring
+            // Is this the best choice? are there others?
+            pkt->setHopCount(pkt->getHopCount() + 1);
+
+            // la idea de este vector es ver que porcentaje de veces elegimos mandar por la gate 0 y cuales por
+            // la gate 1 por lo tanto hay que cambiar aca cuando cambiemos la gate elegida
+            LnkSelectVector.record(0);
+            send(msg, "toLnk$o", 0);
+        }
     }
 }
